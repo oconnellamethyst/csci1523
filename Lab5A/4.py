689e5c0c15d94c1b38a69f02208de31621be0f71
Amethyst O'Connell <oconnellamethyst@gmail.com>
2018-03-27 17:17:54 -0500

    Made it less gross

--- Lab5A/4.py
+++ Lab5A/4.py
@@ -1,12 +1,11 @@
-def poundsToMetric(pounds):
+def poundsToMetric(pounds=float(input("How many pounds? "))):
     # whole number division of a fraction instead of multiplication to keep this number whole
     fraction = 1/0.455
     kilograms = int(pounds // fraction)
     # same with my use of the modulus
-    print(pounds % fraction)
     grams = (pounds % fraction) * 455
 
     print(kilograms, "kilograms, and", grams, "grams")
     return(kilograms, grams)
 
-poundsToMetric(float(input("How many pounds? ")))
+poundsToMetric()
