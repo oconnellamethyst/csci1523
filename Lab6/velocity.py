054786ffba0789bb6b072718f3d9bebc67c09da6
Amethyst O'Connell <oconnellamethyst@gmail.com>
2018-04-10 16:45:53 -0500

    Used wrong quotes

--- Lab6/velocity.py
+++ Lab6/velocity.py
@@ -1,7 +1,7 @@
 # Velocity Module
 import math
 
-'''
+"""
 Function that computes the velocity of a freely falling object subject
 to an acceleration due to gravity, a, from an initial height of x.
 
@@ -16,7 +16,7 @@ Assume x = 100 meters, a = 9.8 meters/(sec^2)
 The velocity on impact with ground would be:
 v = 9.8 * sqrt(2.0*100/9.8)
 v = 44.23 meters per sec
-'''
+"""
 
 def velocity(a,x):
     v = a * math.sqrt(2.0*x/a)
