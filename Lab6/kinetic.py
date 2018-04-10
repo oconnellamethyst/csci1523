2aeb187448e2761a142024557b467a629f967d57
Amethyst O'Connell <oconnellamethyst@gmail.com>
2018-04-10 16:45:29 -0500

    Used wrong quotes

--- Lab6/kinetic.py
+++ Lab6/kinetic.py
@@ -1,5 +1,5 @@
 # Kinetic Energy Module
-'''
+"""
 Function that computes the kinetic energy of mass, m, at a velocity, v.
 
 For the kinetic energy function:
@@ -11,7 +11,7 @@ Assume a mass of 1 kilogram and a velocity of 100 meters per second.
 
 ke = 0.5 * 1 * v ** 2
 ke = 5000 kilogram-meters^2/sec^2
-'''
+"""
 
 def kinetic(v,m):
     ke = 0.5*m*v**2
